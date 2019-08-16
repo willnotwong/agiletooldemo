7cca04b7f6b8fbe7796c287ba68f629f125253bc
William Wong <wwongqwerty@gmail.com>
2019-08-16 10:48:15 -0400

    Updates

---
+++ precache-manifest.e74875245a1fa967e85cfd5326236433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f20bf3e13551387b7315a683b903bc",
+    "url": "/agiletooldemo/index.html"
+  },
+  {
+    "revision": "1b2cc080170c016d7949",
+    "url": "/agiletooldemo/static/css/main.c955f688.chunk.css"
+  },
+  {
+    "revision": "f7411bfa838a688f533d",
+    "url": "/agiletooldemo/static/js/2.cf67c50c.chunk.js"
+  },
+  {
+    "revision": "1b2cc080170c016d7949",
+    "url": "/agiletooldemo/static/js/main.42662ba0.chunk.js"
+  },
+  {
+    "revision": "528f89d6808d8a155a70",
+    "url": "/agiletooldemo/static/js/runtime~main.8fd124c5.js"
+  }
+]);
